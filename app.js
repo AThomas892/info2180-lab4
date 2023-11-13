4a2006ed5a1131c94b0532177e8eeddbce43404b
AThomas892 <alexander.thomas02@mymona.uwi.edu>
2023-11-12 23:43:38 -0500

    Finished work

---
+++ app.js
@@ -0,0 +1,33 @@
+window.onload = function(){
+    let search = document.getElementsByClassName("btn")[0];
+    
+   
+
+    search.addEventListener("click", function(element){
+        element.preventDefault();
+        
+
+        var httpReq = new XMLHttpRequest();
+        var charInput = document.getElementById('character').value;
+        
+        
+        var url = "./superheroes.php";
+
+        httpReq.onreadystatechange = function(){
+            if(httpReq.readyState === XMLHttpRequest.DONE){
+                if(httpReq.status === 200){
+                    document.querySelector("#result").innerHTML = this.responseText;
+                    
+                }
+            
+                else{
+                    alert('There was a problem with the request.');
+                }
+            }
+        }
+        httpReq.open('POST', url);
+        httpReq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        httpReq.send('character=' + encodeURIComponent(charInput));
+        
+    });
+}
